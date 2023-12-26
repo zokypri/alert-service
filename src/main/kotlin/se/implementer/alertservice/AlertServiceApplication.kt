3fdb549b9ac484f39470e2f11030c8a4cba00d0c
zpri <zoky.zp@gmail.com>
2023-12-26 21:22:48 +0700

    initial commit

---
+++ src/main/kotlin/se/implementer/alertservice/AlertServiceApplication.kt
@@ -0,0 +1,11 @@
+package se.implementer.alertservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AlertServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<AlertServiceApplication>(*args)
+}
---
+++ src/test/kotlin/se/implementer/alertservice/AlertServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package se.implementer.alertservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AlertServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
