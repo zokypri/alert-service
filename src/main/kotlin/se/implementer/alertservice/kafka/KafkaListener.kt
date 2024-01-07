cf96cd51c82bf96b5eecad44840d8edf62b06a96
zpri <zoky.zp@gmail.com>
2024-01-07 10:08:54 +0700

    added logs and error handling

--- src/main/kotlin/se/implementer/alertservice/kafka/KafkaListener.kt
+++ src/main/kotlin/se/implementer/alertservice/kafka/KafkaListener.kt
@@ -1,6 +1,7 @@
 package se.implementer.alertservice.kafka
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import java.util.logging.Logger
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
@@ -11,10 +12,19 @@ import se.implementer.alertservice.repository.AlertRepository
 @Component
 class KafkaListener (val alertRepository: AlertRepository){
 
-    @KafkaListener(topics = ["PoliceWarning"])
+    private val logger = Logger.getLogger(KafkaListener::class.java.name)
+
+    @KafkaListener(topics = [Police_WAGNING_TOPIC])
     @Transactional
     fun policeWarningListener(event: String) {
+        logger.info("Received Kafka event from topic $Police_WAGNING_TOPIC with event $event")
         val policeWarning = jacksonObjectMapper().readValue(event, PoliceWarning::class.java)
+        logger.info("Kafka event is mapped and ready to be stored in the DB with event message $policeWarning")
         alertRepository.save(Alert.mapToAlert(policeWarning))
+        logger.info("Kafka event is  stored in the DB table Alert")
+    }
+
+    companion object {
+        private const val Police_WAGNING_TOPIC = "PoliceWarning"
     }
 }
